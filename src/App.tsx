5d107e73c405fa4e143d3e371cd797847c8d74cb
masuim <po.n31227@gmail.com>
2025-03-22 23:14:36 +0900

    feat: Tailwind css の導入と設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-function App() {
-  return <div>Hello world!</div>;
+import "./index.css";
+export default function App() {
+  return (
+    <div className="p-4 bg-red-300">
+      <h1 className="text-3xl font-bold text-blue-600">Hello Tailwind v4!</h1>
+    </div>
+  );
 }
-
-export default App;
