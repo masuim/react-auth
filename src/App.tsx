a8f207f7e05077e7b82353c63028acd0c6ffe048
masuim <po.n31227@gmail.com>
2025-03-23 09:57:36 +0900

    fix: pageディレクトリをsrc直下に移動（アプリケーションの構造を理解しやすくするため）

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./index.css";
 export default function App() {
   return (
     <div className="p-4 bg-red-300">
-      <h1 className="text-3xl font-bold text-blue-600">Hello Tailwind v4!</h1>
+      <h1 className="text-3xl font-bold text-blue-600">Hello world</h1>
     </div>
   );
 }
